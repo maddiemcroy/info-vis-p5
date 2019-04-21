0b98f88716d505f0ae3675768e69c89e1a3a4e92
NateKnob <nate.knauf@gmail.com>
2019-04-21 13:16:17 -0400

    linking!!

--- js/main.js
+++ js/main.js
@@ -11,8 +11,17 @@ $.ajax({
 
 console.log(config);
 
-var width  = 400;
-var height = 350;
+var s_width  = 400;
+var s_height = 350;
+
+var p_margin = {top: 30, right: 10, bottom: 10, left: 0};
+var p_width = 800 - p_margin.left - p_margin.right;
+var p_height = 600 - p_margin.top - p_margin.bottom;
+
+var p_x = d3.scalePoint().range([0, p_width]).padding(1),
+    p_y = {};
+
+var p_dimensions = null;
 
 var genreFilter = "All";
 var timeFilter = {
@@ -20,16 +29,29 @@ var timeFilter = {
   max: "3000"
 };
 
+var line = d3.line(),
+    axisLeft = d3.axisLeft();
+
 d3.csv("./data/movies.csv", function(csv) {
   console.log(csv);
 
-  var svg = d3.select("#chart").append('svg')
-    .attr('width', width)
-    .attr('height', height);
+  var p_background,
+      p_foreground;
 
-  var timeSvg = d3.select("timeScale").append('svg')
-    .attr('width', width)
-    .attr('height', 100);
+  var s_svg = d3.select("#chart").append('svg')
+    .attr('width', s_width)
+    .attr('height', s_height);
+
+  // var timeSvg = d3.select("timeScale").append('svg')
+  //   .attr('width', width)
+  //   .attr('height', 100);
+
+  var p_svg = d3.select("#parallel").append("svg")
+    .attr("width", p_width + p_margin.left + p_margin.right)
+    .attr("height", p_height + p_margin.top + p_margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + p_margin.left + "," + p_margin.top + ")");
+
 
   // for (row :)
   var genreSet = new Set();
@@ -60,18 +82,71 @@ d3.csv("./data/movies.csv", function(csv) {
   // console.log(durationExtent);
   // console.log(timeExtent);
 
-  var xScale = d3.scaleLinear().domain([0, 220]).range([50, width-30]);
-  var yScale = d3.scaleLinear().domain(imdbScoreExtent).range([height-30, 30]);
-  var timeScale = d3.scaleLinear().domain(timeExtent).range([0, width]);
+  var xScale = d3.scaleLinear().domain([0, 220]).range([50, s_width-30]);
+  var yScale = d3.scaleLinear().domain(imdbScoreExtent).range([s_height-30, 30]);
+  var timeScale = d3.scaleLinear().domain(timeExtent).range([0, s_width]);
 
   var xAxis = d3.axisBottom().scale(xScale);
   var yAxis = d3.axisLeft().scale(yScale);
+
+  var valid_keys = ["imdb_score", "duration", "cast_total_facebook_likes", "num_voted_users", "facenumber_in_poster"]
+  p_x.domain(p_dimensions = d3.keys(csv[0]).filter(function(d) {
+    return d != "name" && valid_keys.includes(d) && (p_y[d] = d3.scaleLinear()
+        .domain(d3.extent(csv, function(p) { return +p[d]; }))
+        .range([p_height, 0]));
+  }));
+
+  // p_background = p_svg.append("g")
+  //     .attr("class", "background")
+  //     .selectAll("path")
+  //     .data(csv)
+  //     .enter().append("path")
+  //     .attr("d", path)
+  //     .classed("pc-entry", true);
+
+  // Add blue foreground lines for focus.
+  p_foreground = p_svg.append("g")
+      .attr("class", "foreground")
+      .selectAll("path")
+      .data(csv)
+      .enter().append("path")
+      .classed("pc-entry", true)
+      .attr("d", path)
+      .classed("fg-pc-entry", true);
+
+  // Add a group element for each dimension.
+  const p_g = p_svg.selectAll(".dimension")
+      .data(p_dimensions)
+      .enter().append("g")
+      .attr("class", "dimension")
+      .attr("transform", function(d) { return "translate(" + p_x(d) + ")"; });
+
+  p_g.append("g")
+      .attr("class", "axis")
+      .each(function(d) { d3.select(this).call(axisLeft.scale(p_y[d])); })
+      .append("text")
+      .style("text-anchor", "middle")
+      .attr("y", -9)
+      .text(function(d) { return d; });
+
+  p_g.append("g")
+      .attr("class", "brush")
+      .each(function(d) {
+          d3.select(this).call(p_y[d].brush = d3.brushY()
+            .extent([[-10,0], [10,p_height]])
+            .on("brush", brush)
+            .on("end", brush)
+            )
+        })
+    .selectAll("rect")
+      .attr("x", -8)
+      .attr("width", 16);
 
   var tooltip = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
-  var points = svg.selectAll("circle")
+  var points = s_svg.selectAll("circle")
     .data(csv)
     .enter()
     .append("circle")
@@ -116,17 +191,17 @@ d3.csv("./data/movies.csv", function(csv) {
 
 
 
-  svg.append("g")
-		.attr("transform", "translate(0,"+ (height-30)+ ")")
+  s_svg.append("g")
+		.attr("transform", "translate(0,"+ (s_height-30)+ ")")
 		.call(xAxis)
 		.append("text")
 		.attr("class", "label")
-		.attr("x", width-16)
+		.attr("x", s_width-16)
 		.attr("y", -6)
 		.style("text-anchor", "end")
 		.text("Gross");
 
-  svg.append("g")
+  s_svg.append("g")
 		.attr("transform", "translate(50, 0)")
 		.call(yAxis)
 		.append("text")
@@ -137,25 +212,94 @@ d3.csv("./data/movies.csv", function(csv) {
 		.style("text-anchor", "end")
 		.text("IMDB Score");
 
-
-});
-
-$("#genre-buttons").click((event) => {
-  // console.log(event.target.children[0].value)
-  var radioValue = event.target.children[0].value; //$("input[name='options']:checked").val();
-  if (radioValue == "All") {
-    console.log("its all")
-    d3.selectAll("circle").classed("hidden-circle", false);
-    return
+  function path(d) {
+      return line(p_dimensions.map(function(p) { return [p_x(p), p_y[p](d[p])]; }));
   }
-  // console.log("radio value " + radioValue.toString())
-  d3.selectAll("circle")
-    .classed("hidden-circle", true)
-    .filter((data) => {
-      // console.log(data.genreArray.includes(radioValue))
-      return data.genreArray.includes(radioValue)
-    }).classed("hidden-circle", false)
+
+  var activeBrushes = [];
+
+  function brush() {
+    // var actives = [];
+    activeBrushes = [];
+    p_svg.selectAll(".brush")
+      .filter(function(d) {
+            p_y[d].brushSelectionValue = d3.brushSelection(this);
+            return d3.brushSelection(this);
+      })
+      .each(function(d) {
+          // Get extents of brush along each active selection axis (the Y axes)
+            activeBrushes.push({
+                dimension: d,
+                extent: d3.brushSelection(this).map(p_y[d].invert)
+            });
+      });
+
+    // var selected = [];
+    // Update foreground to only display selected values
+    // p_foreground.style("display", function(d) {
+    //     return actives.every(function(active) {
+    //         let result = active.extent[1] <= d[active.dimension] && d[active.dimension] <= active.extent[0];
+    //         if(result)selected.push(d);
+    //         return result;
+    //     }) ? null : "none";
+    // });
+    updateDisplay();
+    // (actives.length>0)?out.text(d3.tsvFormat(selected.slice(0,24))):out.text(d3.tsvFormat(sample_data.slice(0,24)));;
+  }
+
+  function updateDisplay() {
+
+    function inFocus(data) {
+      // console.log(data.genreArray);
+      return (genreFilter == "All" || data.genreArray.includes(genreFilter))
+        && activeBrushes.every((active) => {
+            let result = active.extent[1] <= data[active.dimension] && data[active.dimension] <= active.extent[0];
+            // if(result)selected.push(d);
+            return result;
+        });
+    }
+
+    d3.selectAll("circle")
+      .classed("hidden-circle", true)
+      .filter(inFocus)
+      .classed("hidden-circle", false);
+
+    d3.selectAll(".foreground")
+      .selectAll("path")
+      .classed("fg-pc-entry", false)
+      .filter(inFocus)
+      .classed("fg-pc-entry", true);
+
+
+    // d3.selectAll("circle")
+    //   .classed("hidden-circle", true)
+    //   .filter((data) => {
+    //     return genreFilter == "All" || data.genreArray.includes(genreFilter)
+    //   })
+    //   .classed("hidden-circle", false);
+    //
+    // d3.selectAll(".foreground")
+    //   .selectAll("path")
+    //   .classed("fg-pc-entry", false)
+    //   .filter((data) => {
+    //     return activeBrushes.every((active) => {
+    //         let result = active.extent[1] <= data[active.dimension] && data[active.dimension] <= active.extent[0];
+    //         // if(result)selected.push(d);
+    //         return result;
+    //     });
+    //   })
+    //   .classed("fg-pc-entry", true);
+  }
+
+  $("#genre-buttons").click((event) => {
+    // console.log(event.target.children[0].value)
+    var radioValue = event.target.children[0].value; //$("input[name='options']:checked").val();
+    genreFilter = radioValue;
+    updateDisplay();
+  });
+
 });
+
 
 
 function getPoster(title) {
@@ -172,125 +316,3 @@ function getPoster(title) {
   });
   return url;
 }
-
-function parallel_coordinates() {
-  d3.csv("./data/movies.csv", function(csv) {
-    var margin = {top: 30, right: 10, bottom: 10, left: 0};
-    var width = 800 - margin.left - margin.right;
-    var height = 600 - margin.top - margin.bottom;
-
-    var x = d3.scalePoint().range([0, width]).padding(1),
-        y = {};
-
-    var line = d3.line(),
-        axis = d3.axisLeft(),
-        background,
-        foreground;
-
-    var dimensions = null;
-
-    const svg = d3.select("#parallel").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g");
-    const svg_adjusted = svg.append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var out = d3.select("#parallel-out")
-    // out.text(d3.tsvFormat(sample_data.slice(0,24)));
-    var sample_data = csv;
-
-    // Extract the list of dimensions and create a scale for each.
-    var valid_keys = ["imdb_score", "duration", "cast_total_facebook_likes", "num_voted_users", "facenumber_in_poster"]
-    x.domain(dimensions = d3.keys(sample_data[0]).filter(function(d) {
-      return d != "name" && valid_keys.includes(d) && (y[d] = d3.scaleLinear()
-          .domain(d3.extent(sample_data, function(p) { return +p[d]; }))
-          .range([height, 0]));
-    }));
-
-    // Add grey background lines for context.
-    background = svg_adjusted.append("g")
-        .attr("class", "background")
-      .selectAll("path")
-        .data(sample_data)
-      .enter().append("path")
-        .attr("d", path)
-        .classed("pc-entry", true);
-
-    // Add blue foreground lines for focus.
-    foreground = svg_adjusted.append("g")
-        .attr("class", "foreground")
-      .selectAll("path")
-        .data(sample_data)
-      .enter().append("path")
-        .attr("d", path)
-        .classed("fg-pc-entry", true);
-
-    // Add a group element for each dimension.
-    const g = svg_adjusted.selectAll(".dimension")
-        .data(dimensions)
-      .enter().append("g")
-        .attr("class", "dimension")
-        .attr("transform", function(d) { return "translate(" + x(d) + ")"; });
-
-    // Add an axis and title.
-    g.append("g")
-        .attr("class", "axis")
-        .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
-      .append("text")
-        .style("text-anchor", "middle")
-        .attr("y", -9)
-        .text(function(d) { return d; });
-
-    // Add and store a brush for each axis.
-    g.append("g")
-        .attr("class", "brush")
-        .each(function(d) {
-            d3.select(this).call(y[d].brush = d3.brushY()
-              .extent([[-10,0], [10,height]])
-              .on("brush", brush)
-              .on("end", brush)
-              )
-          })
-      .selectAll("rect")
-        .attr("x", -8)
-        .attr("width", 16);
-
-    // Returns the path for a given data point.
-    function path(d) {
-        return line(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
-    }
-
-    // Handles a brush event, toggling the display of foreground lines.
-    function brush() {
-        var actives = [];
-        svg.selectAll(".brush")
-          .filter(function(d) {
-                y[d].brushSelectionValue = d3.brushSelection(this);
-                return d3.brushSelection(this);
-          })
-          .each(function(d) {
-              // Get extents of brush along each active selection axis (the Y axes)
-                actives.push({
-                    dimension: d,
-                    extent: d3.brushSelection(this).map(y[d].invert)
-                });
-          });
-
-        var selected = [];
-        // Update foreground to only display selected values
-        foreground.style("display", function(d) {
-            return actives.every(function(active) {
-                let result = active.extent[1] <= d[active.dimension] && d[active.dimension] <= active.extent[0];
-                if(result)selected.push(d);
-                return result;
-            }) ? null : "none";
-        });
-        // (actives.length>0)?out.text(d3.tsvFormat(selected.slice(0,24))):out.text(d3.tsvFormat(sample_data.slice(0,24)));;
-
-    }
-    return svg.node();
-  });
-}
-
-parallel_coordinates();
